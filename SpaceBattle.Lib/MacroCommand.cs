f226e7476fd142ac6b1b3411fad2e3deaf6ba925
chernikowaa <chernikovaaa143@gmail.com>
2023-12-25 19:22:29 +0000

    check

---
+++ SpaceBattle.Lib/MacroCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace SpaceBattle.Lib;
+public class MacroCommand : ICommand
+{
+    public ICommand[] _cmds;
+
+    public MacroCommand(ICommand[] cmds)
+    {
+        _cmds = cmds;
+    }
+
+    public void Execute()
+    {
+        _cmds.ToList().ForEach(cmd => cmd.Execute());
+    }
+}
