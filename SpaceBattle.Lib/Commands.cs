3449ce6c320ddf3a0168f2c38712f868cae97417
chernikowaa <chernikovaaa143@gmail.com>
2024-03-31 12:48:59 +0000

    edit test w scope

--- SpaceBattle.Lib/Commands.cs
+++ SpaceBattle.Lib/Commands.cs
@@ -7,49 +7,49 @@ public interface IMoveable
     public MyVector instant_velocity { get; }
 }
 
-// public class StartCommand : ICommand
-// {
-//     private readonly Order order;
+public class StartCommand : ICommand
+{
+    private readonly Order order;
 
-//     public void Execute()
-//     {
-//         var command = new ContiniousObjectCommand(order.target, order.cmd);
+    public void Execute()
+    {
+        var command = new ContiniousObjectCommand(order.target, order.cmd);
 
-//         order.args.dict.ToList().ForEach(pair =>
-//             order.target.properties.Set(pair.Key, pair.Value)
-//         );
+        order.args.dict.ToList().ForEach(pair =>
+            order.target.properties.Set(pair.Key, pair.Value)
+        );
 
-//         order.target.properties.Set(
-//             order.cmd,
-//             IoC.Resolve<ICommand>(
-//                 order.cmd,
-//                 order.target
-//             )
-//         );
+        order.target.properties.Set(
+            order.cmd,
+            IoC.Resolve<ICommand>(
+                order.cmd,
+                order.target
+            )
+        );
 
-//         IoC.Resolve<IQueue<ICommand>>("Game.Queue").Put(command);
-//     }
+        IoC.Resolve<IQueue<ICommand>>("Game.Queue").Put(command);
+    }
 
-//     public StartCommand(Order order)
-//     {
-//         this.order = order;
-//     }
-// }
+    public StartCommand(Order order)
+    {
+        this.order = order;
+    }
+}
 
-// public class ContiniousObjectCommand : ICommand
-// {
-//     private readonly UObject obj;
-//     private readonly string cmd;
+public class ContiniousObjectCommand : ICommand
+{
+    private readonly UObject obj;
+    private readonly string cmd;
 
-//     public void Execute()
-//     {
-//         ((ICommand)obj.properties.Get(cmd)).Execute();
-//         IoC.Resolve<IQueue<ICommand>>("Game.Queue").Put(this);
-//     }
+    public void Execute()
+    {
+        ((ICommand)obj.properties.Get(cmd)).Execute();
+        IoC.Resolve<IQueue<ICommand>>("Game.Queue").Put(this);
+    }
 
-//     public ContiniousObjectCommand(UObject obj, string cmd)
-//     {
-//         this.obj = obj;
-//         this.cmd = cmd;
-//     }
-// }
+    public ContiniousObjectCommand(UObject obj, string cmd)
+    {
+        this.obj = obj;
+        this.cmd = cmd;
+    }
+}
